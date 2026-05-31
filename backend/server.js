164f628528a001081b817975a71aeef9879d8188
Bro125-dot <giovannicristi11@gmail.com>
2026-05-31 02:58:46 +0200

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,19 @@ import matricerouter from './routes/matrici.js'
 import skidOverviewRoutes from './routes/skidsoverview.js'
 
 const app = express()
+
 app.use(cors())
-const PORT = 3000
 
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT,() => {
-    console.log(`Server avviato sulla porta ${PORT}`)
+app.use('/', robotsrouter)
+app.use('/', matricerouter)
+app.use('/', skidOverviewRoutes)
+
+app.get('/', (req, res) => {
+    res.send('backend funzionante')
 })
 
-app.use('/',robotsrouter)
-app.use('/',matricerouter)
-app.use('/',skidOverviewRoutes)
-app.get('/',(req,res) => {
-    res.send('backend funzionante')
+app.listen(PORT, () => {
+    console.log(`Server avviato sulla porta ${PORT}`)
 })
